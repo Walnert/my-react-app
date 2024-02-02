a3ad5190b17f9e23741d1e0b90b256baa65b77ec
Walnert <psergay3@gmail.com>
2024-02-02 16:04:26 -0500

    Movie Emporium

--- src/App.js
+++ src/App.js
@@ -1,16 +1,79 @@
-//import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
+
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard'
+
+
+// 15ba7908
+// const movie1 = {
+//     "Title": "Shrek",
+//     "Year": "2001",
+//     "imdbID": "tt0126029",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+// }
+const API_URL = "http://omdbapi.com?apikey=15ba7908"
 const App = () => {
-  const [counter, setCounter] = useState(0); // this is a hook
-  return (
-    <div className="App">
-      <button onClick = {() => setCounter(counter + 1)}>+</button>
-      <h1>There {Math.abs(counter) === 1 ? "is" : "are"} {counter} {Math.abs(counter) === 1 ? "way" : "ways"} to skin a cat...?</h1>
-      <button onClick = {() => setCounter(counter - 1)}>-</button>
-    </div>
-  );
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [tooltip, setTooltip] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(`searching for ${title}`);
+        console.log(data.Search);
+        setMovies(data.Search);
+        setTooltip('No movies found. Please try again!')
+
+    }
+
+    useEffect(() => {
+        //searchMovies('shrek');
+        setTooltip('Search for movies!')
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Peter's Movie Emporium</h1>
+            <div className="search">
+                <input
+                    placeholder="Search for movies!"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            searchMovies(searchTerm)
+                        }
+                    }}
+                />
+                <button
+                    className='button'
+                    onClick={() => searchMovies(searchTerm)}
+                >
+                    <img src={SearchIcon} alt="search" />
+                </button>
+            </div>
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="container">
+                            {movies.map((movie) => (
+                                <MovieCard movie={movie} />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>{tooltip}</h2>
+                        </div>
+                    )
+            }
+        </div>
+    );
 }
 
 export default App;
