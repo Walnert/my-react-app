dc4715355eebbec1503d46f97015392e71465474
Walnert <psergay3@gmail.com>
2024-02-02 13:37:30 -0500

    skin a cat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
-  const name = 'Peter';
+  const [counter, setCounter] = useState(0); // this is a hook
   return (
     <div className="App">
-      <h1>Hello, {name}!</h1>
+      <button onClick = {() => setCounter(counter + 1)}>+</button>
+      <h1>There {Math.abs(counter) === 1 ? "is" : "are"} {counter} {Math.abs(counter) === 1 ? "way" : "ways"} to skin a cat...?</h1>
+      <button onClick = {() => setCounter(counter - 1)}>-</button>
     </div>
   );
 }
